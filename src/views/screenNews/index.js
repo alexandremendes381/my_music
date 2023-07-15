8e86711993b1ddb02e67fedbed3413b5deb7fd89
Alexandre Mendes <107935113+alexandremendes381@users.noreply.github.com>
2023-07-15 19:05:34 -0300

    new page

--- src/views/screenNews/index.js
+++ src/views/screenNews/index.js
@@ -1,11 +1,50 @@
-import React from 'react'
-import styles from './index.module.scss'
+import React, { useEffect, useState } from 'react';
+import styles from './index.module.scss';
+import axios from 'axios';
+import { toastInfo, toastSuccess } from '../../utils/ToastInfo';
+
 function ScreenNews() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/posts/')
+      .then(function (response) {
+        console.log(response.data);
+        setPosts(response.data);
+        toastSuccess('Sucesso');
+      })
+      .catch(function (error) {
+        console.log(error);
+        toastInfo('Erro');
+      });
+  }, []);
+
   return (
     <div className={styles.background}>
-      <p>dfgdfgdfgdfgdfgdfgdfgdfgdfgfdgdf</p>
+      <div className={styles.postContainer}>
+        {posts.map((post, index) => (
+          <div className={styles.post} key={post.id}>
+            {index % 2 === 0 ? (
+              <>
+                <div className={styles.textContainer}>
+                  <p className={styles.text}>{post.texto}</p>
+                </div>
+                <img className={styles.image} src={post.arquivo} alt="Imagem da Postagem" />
+              </>
+            ) : (
+              <>
+                <img className={styles.image} src={post.arquivo} alt="Imagem da Postagem" />
+                <div className={styles.textContainer}>
+                  <p className={styles.text}>{post.texto}</p>
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ScreenNews
+export default ScreenNews;
--- src/views/sidebarMenu/index.js
+++ src/views/sidebarMenu/index.js
@@ -1,41 +1,67 @@
 import React, { useState } from 'react';
 import styles from './index.module.scss';
 import { useNavigate } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 import ScreenBiblioteca from '../screenBiblioteca';
 
 function SidebarMenu({ tracks, currentTrackIndex, handleTrackClick }) {
+  const [isOpen, setIsOpen] = useState(false);
   const [isOpenBiblioteca, setIsBiblioteca] = useState(false);
-
-
 
   const Home = useNavigate();
   const Video = useNavigate();
+  const Menu = useNavigate();
+
+  const onClickMenuPrincipal = () => {
+    Menu('/MenuPrincipal')
+  };
+
   const handleButtonClickHome = () => {
     Home('/');
   };
+
   const handleButtonClickVideo = () => {
     Video('/video');
   };
 
-  const hendleClickBiblioteca = () => {
-    setIsBiblioteca(true)
-  }
+  const handleClickBiblioteca = () => {
+    setIsBiblioteca(true);
+    setIsOpen(false);
+  };
+
+  const handleSidebarToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div>
-      <div className={styles.sidebar}>
+    <div className={styles.sidebarContainer}>
+      <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
         <ul className={styles.sidebarmenu}>
           <li>
-            <a href="#" onClick={handleButtonClickHome}>Início</a>
+            <a href="#" onClick={handleButtonClickHome}>
+              Sair
+            </a>
           </li>
           <li>
-            <a href="#" onClick={hendleClickBiblioteca}>Biblioteca</a>
+          <a href="#" onClick={onClickMenuPrincipal}>
+            Menu Principal
+          </a>
+        </li>
+          <li>
+            <a href="#" onClick={handleClickBiblioteca}>
+              Biblioteca
+            </a>
           </li>
           <li>
-            <a href="#" onClick={handleButtonClickVideo}>Assistir Videos</a>
+            <a href="#" onClick={handleButtonClickVideo}>
+              Assistir Videos
+            </a>
           </li>
         </ul>
       </div>
+      <button className={styles.sidebarToggle} onClick={handleSidebarToggle}>
+        <FaBars />
+      </button>
       <div>
         <ScreenBiblioteca
           tracks={tracks}
